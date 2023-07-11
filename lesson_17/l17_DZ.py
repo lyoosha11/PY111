82dcb79432fb0c931602c8e52d5b2ab5316a18f9
lyoosha11 <810lyosha1118@gmail.com>
2023-07-11 22:30:30 +0300

    write code DZ of OOP

---
+++ lesson_17/l17_DZ.py
@@ -0,0 +1,42 @@
+class DZ:
+
+    def __init__(self):
+        self.data = input('Enter somewhere: ')
+
+    @property
+    def processing(self):
+        string = []
+        integer = []
+        default_glas = ['a', 'e', 'i', 'y', 'u', 'o']
+        glas = []
+        soglas = []
+        summ = 0
+        try:
+            self.data = int(self.data)
+            for i in str(self.data):
+                integer.append(int(i))
+        except ValueError:
+            for i in self.data:
+                string.append(i.lower())
+        if string:
+            for i in string:
+                if i in default_glas:
+                    glas.append(i)
+                else:
+                    soglas.append(i)
+            if len(glas) * len(soglas) <= self.length():
+                return glas
+            else:
+                return soglas
+        else:
+            for i in integer:
+                if int(i) % 2 == 0:
+                    summ += int(i)
+            return summ * self.length()
+
+    def length(self):
+        return len(str(self.data))
+
+
+task = DZ()
+print(task.processing)
